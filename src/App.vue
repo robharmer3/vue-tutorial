d3cf837f32d30e7e33da38bb9826f038b3b3cad6
robharmer3 <rob.harmer3@gmail.com>
2025-07-22 14:45:58 +0100

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+import CButton from "./components/CButton.vue";
+
+const count = ref(0);
+
+const countUp = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <div>
+    <h1>Hello Rob's Counter!</h1>
+    <CButton @click="countUp">Click Me!</CButton>
+  </div>
+  <div>
+    {{ count }}
+  </div>
+</template>
---
+++ src/components/CButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background: red;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
